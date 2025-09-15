717b1c5179742f3b423c690e488b6da962736bac
Alexis Trujillo <alextrum04@gmail.com>
2025-09-15 17:32:43 -0600

    Agrego regularizacion L1 y L2

--- run_network_keras.py
+++ run_network_keras.py
@@ -48,17 +48,17 @@ y_trainc = keras.utils.to_categorical(y_train, num_classes)
 y_testc = keras.utils.to_categorical(y_test, num_classes)
 
 
-with mlflow.start_run(run_name="tanh"):
+with mlflow.start_run(run_name="tanh_l1_l2"):
     
     model = Sequential()
     model.add(Dense(512, 
                     activation='tanh', 
                     input_shape=(784,), 
-                    kernel_regularizer=regularizers.l2(0.001)))
+                    kernel_regularizer=regularizers.l1_l2(l1=0.001, l2=0.001)))
     
     model.add(Dense(256,
                      activation='tanh',
-                     kernel_regularizer=regularizers.l2(0.001)))
+                     kernel_regularizer=regularizers.l1_l2(l1=0.001, l2=0.001)))
     
     model.add(Dense(num_classes, activation='softmax'))                      
     
@@ -96,5 +96,5 @@ with mlflow.start_run(run_name="tanh"):
     mlflow.log_metric("test_loss", test_loss)
 
     
-    mlflow.keras.save_model(model, "-modelo-2")
-    mlflow.log_artifacts("-modelo-2", artifact_path="-modelo-2")
+    mlflow.keras.save_model(model, "-modelo-3")
+    mlflow.log_artifacts("-modelo-3", artifact_path="-modelo-3")
