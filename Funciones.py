0591e87eac1362146bb62cb09fe5485b5fde41c0
alextrum04-bit <alextrum04@gmail.com>
2025-09-29 08:03:06 -0600

    Agrego cambios al codigo para reproducir la funcion  1 + 2*x + 4*tf.pow(x, 3)

--- Funciones.py
+++ Funciones.py
@@ -18,9 +18,9 @@ class Funsol(keras.Model):
         return [loss_tracker] #igual cambia el loss_tracker
 
     def train_step(self, data):
-        batch_size =10 #Calibra la resolucion de la ec.dif
+        batch_size = 20 #Calibra la resolucion de la ec.dif
         x = tf.random.uniform((batch_size,), minval=-1, maxval=1)
-        eq = 3*tf.math.sin(math.pi*x) 
+        eq = 1 + 2*x + 4*tf.pow(x, 3)
 
         with tf.GradientTape() as tape:
             y_pred = self(x, training=True)
@@ -35,54 +35,27 @@ class Funsol(keras.Model):
 
 
 
-class SinTransform(tf.keras.layers.Layer):
-    def __init__(self, num_outputs):
-        super(SinTransform,self).__init__()
-        self.num_outputs = num_outputs
-
-        self.freq = tf.range(1., self.num_outputs + 1)
-
-        self.kernel = self.add_weight(name="kernel",
-                                shape=[self.num_outputs])
-
-    def call(self, inputs):
-        inputs = tf.convert_to_tensor(inputs)
-        if (inputs.shape == ()):
-            inputs=(inputs,)
-        elif (len(inputs.shape)==1):
-            inputs=tf.expand_dims(inputs, axis=1)
-        batch = tf.shape(inputs)[0]
-        self.freq_b = tf.ones([batch,1])*self.freq
-        args = self.freq_b * inputs
-        modes = tf.math.sin(args)
-        return tf.tensordot(modes,self.kernel,1)
-    
-
-trans = SinTransform(15)
-x = tf.random.uniform((5,), minval=-1, maxval=1)
-print(x)
-res=trans(x)
-print(res)
-
-
 inputs = keras.Input(shape=(1,))
-print(inputs)
-x = SinTransform(15)(inputs)
-model = Funsol(inputs=inputs,outputs=x)
+x = keras.layers.Dense(64, activation="tanh")(inputs)
+x = keras.layers.Dense(64, activation="tanh")(x)
+outputs = keras.layers.Dense(1)(x)
+
+model = Funsol(inputs=inputs, outputs=outputs)
+model.compile(optimizer=keras.optimizers.Adam(0.01))
 model.summary()
 
 
-model.compile(optimizer=SGD(learning_rate=0.01), metrics=['loss'])
+history = model.fit(x=tf.zeros((1,1)), y=tf.zeros((1,1)), epochs=100, verbose=1)
 
-x=tf.linspace(-1,1,100)
-history = model.fit(x,epochs=100,verbose=1)
+x_test = np.linspace(-1, 1, 100).reshape(-1, 1)
+y_true = 1 + 2*x_test + 4*(x_test**3)
+y_pred = model.predict(x_test)
 
-model.summary()
-print(model.layers[1].trainable_weights)
-
-x_testv = tf.linspace(-1,1,100)
-a=model.predict(x_testv)
-
-plt.plot(x_testv,a)
-plt.plot(x_testv, 3*tf.math.sin(math.pi*x) )
+plt.figure(figsize=(8,5))
+plt.plot(x_test, y_true, label="Función real: 1+2x+4x³", color="black")
+plt.plot(x_test, y_pred, "--", label="Predicción red", color="red")
+plt.legend()
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.title("Aproximación de 1 + 2x + 4x³ con Funsol")
 plt.show()
